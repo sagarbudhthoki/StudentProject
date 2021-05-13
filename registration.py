b389bea8a6fe8a2a6937fda5f84b0cc1859986c3
Sagar Budhathoki <sagarbudhathoki128@gmail.com>
2021-05-13 20:51:05 +0545

    My first commit

--- registration.py
+++ registration.py
@@ -84,7 +84,7 @@ class Register:
 
             try:
 
-                con=pymysql.connect(host="localhost",user="root",password="",database="register")
+                con=pymysql.connect(host="localhost",user="root",password="sagar123",database="register")
                 if con:
                     print("server info",con.get_server_info())
                 cur=con.cursor()
@@ -98,15 +98,15 @@ class Register:
                 if row!=None:
                     messagebox.showerror("Error", "User already Exist,Please try with another email", parent=self.root)
                 else:
-                    cur.execute("insert into register(f_name,l_name,contact,email,question,answer,password)values(%s%s%s%s%s%s%s",
-                            (self.txt_fname.get(),
-                             self.txt_lname.get(),
-                             self.txt_contact.get(),
-                             self.txt_email.get(),
-                             self.cmb_quest.get(),
-                             self.txt_answer.get(),
-                             self.txt_password.get()
-                             ))
+                    cur.execute("insert into register (f_name,l_name,contact,email,question,answer,password) values(%s,%s,%s,%s,%s,%s%,%s",
+                                    (self.txt_fname.get(),
+                                    self.txt_lname.get(),
+                                    self.txt_contact.get(),
+                                    self.txt_email.get(),
+                                    self.cmb_quest.get(),
+                                    self.txt_answer.get(),
+                                    self.txt_password.get()
+                                    ))
                 con.commit()
                 con.close()
                 messagebox.showinfo("Success", "Successfully Registered", parent=self.root)
